ef4035f5b16ff5541b180524eb268becb0eb19ae
Austin <austin.x.wng@gmail.com>
2022-09-25 17:18:49 -0500

    Assignment 3

--- app/src/main/java/com/example/myfirstapp/CurrentConditionsFragment.kt
+++ app/src/main/java/com/example/myfirstapp/CurrentConditionsFragment.kt
@@ -2,6 +2,7 @@ package com.example.myfirstapp
 
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.myfirstapp.databinding.FragmentCurrentConditionsBinding
@@ -12,11 +13,10 @@ class CurrentConditionsFragment : Fragment(R.layout.fragment_current_conditions)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        (activity as AppCompatActivity).supportActionBar?.title = resources.getString(R.string.app_name)
         binding = FragmentCurrentConditionsBinding.bind(view)
         binding.forecastButton.setOnClickListener {
-            val dayForecast = DayForecast("72")
-            val action = CurrentConditionsFragmentDirections.actionCurrentConditionsFragmentToForecastFragment(dayForecast)
-            findNavController().navigate(action)
+            findNavController().navigate(R.id.action_currentConditionsFragment_to_forecastFragment)
         }
     }
 }
--- app/src/main/java/com/example/myfirstapp/DayForecast.kt
+++ app/src/main/java/com/example/myfirstapp/DayForecast.kt
@@ -4,4 +4,10 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class DayForecast(val temp: String) : Parcelable
+data class DayForecast(
+    val date: Long,
+    val sunrise: Long,
+    val sunset: Long,
+    val temp: ForecastTemp,
+    val pressure: Float,
+    val humidity: Int) : Parcelable
